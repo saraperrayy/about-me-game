029eb9e5498d7d8cd96a85d83ef14e5962e59da9
SARA PERRY <SARAPERRYY@GMAIL.COM>
2021-04-14 16:35:28 -0500

    initial commit, removed notes from class, added first round of Q&A

--- js/app.js
+++ js/app.js
@@ -1,35 +1,58 @@
 'use strict';
 // If conditional based confirm command
-let answer1 = confirm('Are you ready to rumble?')
+
+// allow the user to input their name for a personalized experience
+let username = window.prompt("Welcome to my site. What is your name?")
+// Check to see if a username is Sara
+if(username === '') {
+    document.write('Enter your username')
+} else {
+     document.write(`<h1>Thank you ${username}</h1>`)
+}
+alert( 'Let\'s see what you can guess about me!')
+
+let answer1 = confirm('Are you ready to play a game?')
 if (answer1 === true) {
-  console.log('LET\'S GET READY TO RUMBLE!!!')
+  /*console.log('Great! Let\'s begin!')*/
+  alert('Great! Let\'s begin!')
 } else {
-  console.log('It\'s above me now.')
+  /*console.log('See you again soon.')*/
+  alert('See you again soon.')
 }
-// logical operator
-let myBool = false;
-let anotherBool = false;
-let thirdBool = false;
-if (myBool && thirdBool) {
-  console.log('myBool and thirdBool were both true');
-} else if (myBool || anotherBool) {
-  console.log('My bool or anotherBool was true!');
-} else if (thirdBool) {
-  console.log('myBool and anotherBool were false, but thirdBool was true.')
-} else {
-  console.log('Were any of my variables true, cuh?');
-}
-let color = prompt('what is your favorite color');
+let school = prompt('Did Sara go to Middle Tennessee State University?')
 // convert the variable color to lower case to evaluate it
-switch (color.toLowerCase()) {
-  case 'red': 
-  alert('your favorite color was red!');
-//break statement follows each case to 'break' the loop not needed if return is present
-  break;
-  case 'blue':
-  alert('your favorite color was blue!');
-  break;
-// default statement is what is fired in there event none of the cases occur
-  default:
-  alert('I don\'t know what your favorite color was ¯\\_(ツ)_/¯');
-}
+if (school.toLowerCase() === 'yes') {
+  alert('You\'re correct!')
+  } else {
+    alert('Sorry, Sara is a True Blue alum')
+  }
+  let schoolyear = prompt('Did she graduate from MTSU in 2018 or 2020?')
+  if (schoolyear.toLowerCase() === '2020') {
+  alert(' Correct! Sara just graduated with a Bachelors of Science degree in Journalism!')
+  } else {
+      alert('Sorry, you missed the question!')
+  }  
+let state = prompt('Does Sara live in Tennessee or California?')
+    if (state.toLowerCase() === 'tennessee') {
+    alert(' Correct! Sara lives in the Volunteer State!')
+    } else {
+        alert('That is incorrect, maybe one day!')
+    }
+let job = prompt('Did she work for a pharmacy?')
+    if (job.toLowerCase() === 'yes') {
+    alert(' Correct!')
+    } else {
+        alert('Sorry — maybe you\'ll get the next one right!')
+    }
+let business = prompt('What year did she start Gemini Design Studio?')
+    if (business.toLowerCase() === '2021') {
+    alert(' Correct!')
+    } else {
+        alert('Sorry, friend — that\'s incorrect.')
+    }
+let network = prompt('Did she work for Women In The Lead?')
+    if (network.toLowerCase() === 'no') {
+    alert(' Great work! You are correct!')
+    } else {
+        alert('Sorry! That was a trick question. Sara was only a professional member of this organization.')
+    }
